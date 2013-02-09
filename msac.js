4a4b8cff91a854ea580c0e59ef13f1db78f731cf
Florian Margaine <florian@margaine.com>
2013-02-09 11:34:17 +0100

    almost full-featured.

--- msac.js
+++ msac.js
@@ -30,8 +30,7 @@ selection.appendTo('body');
 input.on('keyup', function(e) {
     if (e.keyCode === 13) {
         // Pressing enter: reset the value and add the selection
-        this.value = '';
-        selection.append(suggestions.children().first());
+        addToSelection($('.selected', suggestions));
         return false;
     }
     suggestions.children().remove();
@@ -40,28 +39,45 @@ input.on('keyup', function(e) {
         $.each(idxs, function(i) {
             var newSugg = sugg.clone();
             if (i === 0) {
-                newSugg.css('background', 'pink');
+                newSugg.addClass('selected');
             }
+            newSugg.data('idx', this);
             newSugg.text(options[this]).appendTo(suggestions);
         });
     }
 });
 
+suggestions.on('mouseover',  'div', function() {
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+});
+
+suggestions.on('click', 'div', function() {
+    addToSelection($(this));
+});
+
+function addToSelection(el) {
+    input.value = '';
+    el.removeClass('selected');
+    selection.append(el);
+    s[0].options[el.data('idx')].selected = true;
+}
+
 function findInOptions(text) {
     var idxs = [],
         t = text.toLowerCase();
-    
+
     if (t) {
         $.each(options, function(i) {
             var opt = this.toLowerCase(),
                 idx = opt.indexOf(t);
-            
+
             if (~idx) {
                 idxs.push(i);
             }
         });
     }
-    
+
     // "if ([])" passes... we don't want that
     return idxs.length ? idxs : null;
 }
